5d94ed40c200d367cb61147e44cc98c89c85177b
Keith Ricker <keithricker@gmail.com>
2020-08-10 18:24:38 -0500

    updating index file

---
+++ index.js
@@ -0,0 +1,19 @@
+const { Objekt } = require('./module/Objekt')
+const konstructor = require('./module/konstructor')
+const { Mirror, mirrors } = require('./module/Mirror')
+const Chars = require('./module/Chars')
+const funktion = require('./module/funktion')
+const { history, Lineage } = require('./module/utils')
+const klass = require('./module/klass')
+const Tree = require('./module/Tree')
+
+exports.Objekt = Objekt
+exports.konstructor = konstructor
+exports.Mirror = Mirror
+exports.mirrors = mirrors
+exports.Chars = Chars
+exports.funktion = funktion
+exports.history = history
+exports.Lineage = Lineage
+exports.klass = klass
+exports.Tree = Tree
